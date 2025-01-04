b2816179d27a646983edc5c6ffd2348d0f344cd1
Aaron Brethorst <aaron@brethorsting.com>
2025-01-04 15:02:58 -0800

    Plumb some sample data into the ping API call

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,63 +1,83 @@
 package main
 
 import (
-    "flag"
-    "fmt"
-    "log/slog"
-    "net/http"
-    "os"
-    "time"
+	"flag"
+	"fmt"
+	"log/slog"
+	"net/http"
+	"os"
+	"time"
 )
 
-// Declare a string containing the application version number. Later in the book we'll 
+// Declare a string containing the application version number. Later in the book we'll
 // generate this automatically at build time, but for now we'll just store the version
 // number as a hard-coded global constant.
 const version = "1.0.0"
 
+type ObaServer struct {
+	Name               string
+	ID                 int
+	URL                string
+	GtfsUrl            string
+	TripUpdateUrl      string
+	VehiclePositionUrl string
+}
+
 // Define a config struct to hold all the configuration settings for our application.
-// For now, the only configuration settings will be the network port that we want the 
+// For now, the only configuration settings will be the network port that we want the
 // server to listen on, and the name of the current operating environment for the
-// application (development, staging, production, etc.). We will read in these  
+// application (development, staging, production, etc.). We will read in these
 // configuration settings from command-line flags when the application starts.
 type config struct {
-    port int
-    env  string
+	port    int
+	env     string
+	servers map[int]ObaServer
 }
 
 // Define an application struct to hold the dependencies for our HTTP handlers, helpers,
-// and middleware. At the moment this only contains a copy of the config struct and a 
+// and middleware. At the moment this only contains a copy of the config struct and a
 // logger, but it will grow to include a lot more as our build progresses.
 type application struct {
-    config config
-    logger *slog.Logger
+	config config
+	logger *slog.Logger
 }
 
 func main() {
-    var cfg config
+	var cfg config
 
-    flag.IntVar(&cfg.port, "port", 4000, "API server port")
-    flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
-    flag.Parse()
+	flag.IntVar(&cfg.port, "port", 4000, "API server port")
+	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
+	flag.Parse()
 
-    logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+	cfg.servers = make(map[int]ObaServer)
+	cfg.servers[1] = ObaServer{
+		Name:               "Sound Transit",
+		ID:                 1,
+		URL:                "https://api.pugetsound.onebusaway.org",
+		GtfsUrl:            "https://www.soundtransit.org/GTFS-rail/40_gtfs.zip",
+		TripUpdateUrl:      "https://api.pugetsound.onebusaway.org/api/gtfs_realtime/trip-updates-for-agency/40.pb?key=org.onebusaway.iphone",
+		VehiclePositionUrl: "https://api.pugetsound.onebusaway.org/api/gtfs_realtime/vehicle-positions-for-agency/40.pb?key=org.onebusaway.iphone",
+	}
 
-    app := &application{
-        config: cfg,
-        logger: logger,
-    }
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 
-    // Use the httprouter instance returned by app.routes() as the server handler.
-    srv := &http.Server{
-        Addr:         fmt.Sprintf(":%d", cfg.port),
-        Handler:      app.routes(),
-        IdleTimeout:  time.Minute,
-        ReadTimeout:  5 * time.Second,
-        WriteTimeout: 10 * time.Second,
-        ErrorLog:     slog.NewLogLogger(logger.Handler(), slog.LevelError),
-    }
+	app := &application{
+		config: cfg,
+		logger: logger,
+	}
 
-    logger.Info("starting server", "addr", srv.Addr, "env", cfg.env)
-    err := srv.ListenAndServe()
-    logger.Error(err.Error())
-    os.Exit(1)
+	// Use the httprouter instance returned by app.routes() as the server handler.
+	srv := &http.Server{
+		Addr:         fmt.Sprintf(":%d", cfg.port),
+		Handler:      app.routes(),
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		ErrorLog:     slog.NewLogLogger(logger.Handler(), slog.LevelError),
+	}
+
+	logger.Info("starting server", "addr", srv.Addr, "env", cfg.env)
+	err := srv.ListenAndServe()
+	logger.Error(err.Error())
+	os.Exit(1)
 }
--- cmd/api/ping.go
+++ cmd/api/ping.go
@@ -2,22 +2,29 @@ package main
 
 import (
 	"fmt"
-    "net/http"
-    "strconv" 
+	"net/http"
+	"strconv"
 
-    "github.com/julienschmidt/httprouter"
+	"github.com/julienschmidt/httprouter"
 )
 
-// Declare a handler which writes a plain-text response with information about the 
+// Declare a handler which writes a plain-text response with information about the
 // application status, operating environment and version.
 func (app *application) pingMetricHandler(w http.ResponseWriter, r *http.Request) {
 	params := httprouter.ParamsFromContext(r.Context())
-	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
-    if err != nil || id < 1 {
-        http.NotFound(w, r)
-        return
-    }
+	id, err := strconv.Atoi(params.ByName("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
 
-    // Otherwise, interpolate the service ID in a placeholder response.
-    fmt.Fprintf(w, "show the details of service ID %d\n", id)
+	server, exists := app.config.servers[id]
+
+	if !exists {
+		http.NotFound(w, r)
+		return
+	}
+
+	// Otherwise, interpolate the service ID in a placeholder response.
+	fmt.Fprintf(w, "show the details of server %s (ID %d)\n", server.Name, server.ID)
 }
