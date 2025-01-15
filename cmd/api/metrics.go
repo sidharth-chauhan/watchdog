f4b2c3b94fae14ce3b273e790b32057e207bf5c2
Ahmedhossamdev <ahmedhossamdev1@gmail.com>
2025-01-15 19:09:07 +0200

    Refactor metrics collection to use the last cached file and add agency coverage checks

--- cmd/api/metrics.go
+++ cmd/api/metrics.go
@@ -4,6 +4,7 @@ import (
 	"time"
 
 	"watchdog.onebusaway.org/internal/metrics"
+	"watchdog.onebusaway.org/internal/utils"
 )
 
 func (app *application) startMetricsCollection() {
@@ -16,10 +17,40 @@ func (app *application) startMetricsCollection() {
 					metrics.ServerPing(server)
 				}
 
-				cachePath := "cache/gtfs.zip"
-				_, _, err := metrics.CheckBundleExpiration(cachePath, app.logger)
+				cachePath, err := utils.GetLastCachedFile("cache")
+
+				if err != nil {
+					app.logger.Error("Failed to get last cached file", "error", err)
+					continue
+				}
+
+				_, _, err = metrics.CheckBundleExpiration(cachePath, app.logger)
 				if err != nil {
 					app.logger.Error("Failed to check GTFS bundle expiration", "error", err)
+				}
+
+				staticAgenciesNumber, err := metrics.CheckAgenciesWithCoverage(cachePath, app.logger)
+				if err != nil {
+					app.logger.Error("Failed to check agencies with coverage", "error", err)
+					continue
+				}
+
+				numberOfAgencies, err := metrics.GetAgenciesWithCoverage(app.config.Servers[0])
+				if err != nil {
+					app.logger.Error("Failed to get agencies with coverage", "error", err)
+					continue
+				}
+
+				difference := staticAgenciesNumber - numberOfAgencies
+
+				metrics.AgenciesDifference.Set(float64(difference))
+
+				// to verify that the number of agencies in the static GTFS file matches the number of agencies in the agencies-with-coverage endpoint
+				// use this query in Prometheus: oba_agencies_difference == bool 0
+				if difference != 0 {
+					app.logger.Error("Number of agencies with coverage does not match", "static", staticAgenciesNumber, "oba", numberOfAgencies)
+				} else {
+					app.logger.Info("Number of agencies matches", "static", staticAgenciesNumber, "oba", numberOfAgencies)
 				}
 			}
 		}
