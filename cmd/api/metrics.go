d0220b7f0ef870a1a99d51f84ecdca4cb8609397
Ahmedhossamdev <ahmedhossamdev1@gmail.com>
2025-01-16 01:26:34 +0200

    Remove unused checks

--- cmd/api/metrics.go
+++ cmd/api/metrics.go
@@ -29,25 +29,11 @@ func (app *application) startMetricsCollection() {
 					app.logger.Error("Failed to check GTFS bundle expiration", "error", err)
 				}
 
-				staticAgenciesNumber, err := metrics.CheckAgenciesWithCoverage(cachePath, app.logger, app.config.Servers[0])
+				_, err = metrics.CheckAgenciesWithCoverage(cachePath, app.logger, app.config.Servers[0])
+
 				if err != nil {
 					app.logger.Error("Failed to check agencies with coverage", "error", err)
-					continue
 				}
-
-				numberOfAgencies, err := metrics.GetAgenciesWithCoverage(app.config.Servers[0])
-				if err != nil {
-					app.logger.Error("Failed to get agencies with coverage", "error", err)
-					continue
-				}
-
-				difference := staticAgenciesNumber - numberOfAgencies
-
-				metrics.AgenciesDifference.WithLabelValues(app.config.Servers[0].ObaBaseURL).Set(float64(difference))
-
-				// to verify that the number of agencies in the static GTFS file matches the number of agencies in the agencies-with-coverage endpoint
-				// use this query in Prometheus: oba_agencies_difference == bool 0
-				metrics.AgenciesDifference.WithLabelValues(app.config.Servers[0].ObaBaseURL).Set(float64(difference))
 			}
 		}
 	}()
